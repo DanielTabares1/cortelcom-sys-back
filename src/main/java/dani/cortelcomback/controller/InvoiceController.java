5cdf55ad32a4ef4e04b79a4bf2f62fb6085f95f7
Daniel Tabares <daniel.tabares1@udea.edu.co>
2024-01-27 13:24:53 -0500

    Added basic endpoints for invoices

--- src/main/java/dani/cortelcomback/controller/InvoiceController.java
+++ src/main/java/dani/cortelcomback/controller/InvoiceController.java
@@ -5,9 +5,7 @@ import dani.cortelcomback.model.Invoice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,37 @@ public class InvoiceController {
     public ResponseEntity<List<Invoice>> listInvoices(){
         List<Invoice> invoices = invoiceService.findAllInvoices();
         return ResponseEntity.ok(invoices);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Invoice> findInvoiceById(@PathVariable int id){
+        Invoice invoice = invoiceService.findInvoiceById(id);
+        return ResponseEntity.ok(invoice);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Invoice> addInvoice(@RequestBody Invoice invoice){
+        Invoice addedInvoice = invoiceService.saveInvoice(invoice);
+        return ResponseEntity.ok(addedInvoice);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Invoice> editInvoice(@PathVariable int id, @RequestBody Invoice invoice){
+        Invoice existingInvoice = invoiceService.findInvoiceById(id);
+        existingInvoice.setAmount_billed(invoice.getAmount_billed());
+        existingInvoice.setClient(invoice.getClient());
+        existingInvoice.setService(invoice.getService());
+        existingInvoice.setGenerationDate(invoice.getGenerationDate());
+        existingInvoice.setPayment_limit_date(invoice.getPayment_limit_date());
+        invoiceService.saveInvoice(existingInvoice);
+        return ResponseEntity.ok(existingInvoice);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Invoice> deleteInvoice(@PathVariable int id){
+        Invoice invoice = invoiceService.findInvoiceById(id);
+        invoiceService.deleteInvoice(invoice);
+        return ResponseEntity.ok(invoice);
     }
 
 }
